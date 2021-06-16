5c36b8994468bb88d33aa7c2458ecd8e3e9277f5
elliscwc <29953232+elliscwc@users.noreply.github.com>
2021-06-16 09:49:23 +0800

    feat: Android changes

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -117,7 +117,9 @@ public class ReactEditText extends AppCompatEditText
   private final @Nullable FabricViewStateManager mFabricViewStateManager =
       new FabricViewStateManager();
   protected boolean mDisableTextDiffing = false;
-
+  
+  // @Taskadev1 Editor input prop declaration
+  protected boolean mIsEditorInput = false;
   protected boolean mIsSettingTextFromState = false;
 
   private static final KeyListener sKeyListener = QwertyKeyListener.getInstanceForFullKeyboard();
@@ -384,6 +386,11 @@ public class ReactEditText extends AppCompatEditText
 
   public void setOnKeyPress(boolean onKeyPress) {
     mOnKeyPress = onKeyPress;
+  }
+  
+  // @Taskadev1 Set editor input prop
+  public void setIsEditorInput(boolean isEditorInput) {
+    mIsEditorInput = isEditorInput;
   }
 
   public boolean getBlurOnSubmit() {
