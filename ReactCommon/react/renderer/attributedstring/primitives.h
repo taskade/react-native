6206a127c7d3f94e97e7e1f01d7465ee5ef58a37
Amos Tan <alphatrl1997@gmail.com>
2023-07-25 10:16:40 +0800

    feat(ios-text): Add new `textDecorationStyle`
    * 'taskade' with thicker underline style

--- ReactCommon/react/renderer/attributedstring/primitives.h
+++ ReactCommon/react/renderer/attributedstring/primitives.h
@@ -81,7 +81,7 @@ enum class TextDecorationLineType {
   UnderlineStrikethrough
 };
 
-enum class TextDecorationStyle { Solid, Double, Dotted, Dashed };
+enum class TextDecorationStyle { Solid, Double, Dotted, Dashed, Taskade };
 
 enum class AccessibilityRole {
   None,
